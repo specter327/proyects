c0a35fa8d6d389ac7ec77ab866a8ad01c72cb273
specter327 <danielp.mcontact@gmail.com>
2026-01-28 10:21:31 -0600

    Se añaden excepciones estandar

--- DataValue/src/exceptions/__init__.py
+++ DataValue/src/exceptions/__init__.py
@@ -1,1 +1,10 @@
+# Classes definition
+# Base exception
+class DataValueException(Exception): pass
 
+# Specific exceptions
+class DataTypeException(DataValueException): pass
+class LengthException(DataValueException): pass
+class SizeException(DataValueException): pass
+class PossibleValueException(DataValueException): pass
+class RegularExpressionException(DataValueException): pass
